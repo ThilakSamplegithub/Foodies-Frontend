c7d449de0b16afea8714ff1f07dba5c486cc7f00
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2024-06-29 22:23:11 +0530

    added all sensitive information of firebase in env file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -9,13 +9,14 @@ import {
 } from "firebase/auth"
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCU0MEiIRSsOOU-vOT2yIYNcK6bQj2zOJo",
-  authDomain: "foodies-paradise-firebase-auth.firebaseapp.com",
-  projectId: "foodies-paradise-firebase-auth",
-  storageBucket: "foodies-paradise-firebase-auth.appspot.com",
-  messagingSenderId: "263628984933",
-  appId: "1:263628984933:web:38a90a34b361c127422c8c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
