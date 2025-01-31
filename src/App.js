35fae306c8a1325a5a45f230c71c7b3b420f2dff
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2025-01-31 13:09:48 +0530

    connected github to netlify for ci/cd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import { MainRoutes } from './Pages/MainRoutes';
+
 // Main routes is also a page
 // use custom hooks and develop logics in custom hooks to get promoted in awesome way
 
